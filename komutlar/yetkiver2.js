967c20505a4aac0b212b6a997596b0da3172b939
Adonciafx <105149001+Adonciafx@users.noreply.github.com>
2022-06-06 11:11:33 +0300

    Update yetkiver2.js

--- komutlar/yetkiver2.js
+++ komutlar/yetkiver2.js
@@ -4,9 +4,8 @@ const moment = require("moment")
 moment.locale("tr")  
 exports.run = async(client, message, args) => {
 if (!message.member.roles.cache.has(ayarlar.yetkilialımdm) && !message.member.hasPermission('ADMINISTRATOR')) return message.react("❌")
-let Adoncia = message.mentions.users.first()
-if (!Adoncia) return message.react("❌")
-let member = message.guild.member(Adoncia)
+let member = message.mentions.users.first() || message.guild.members.cache.get(args[0]);
+if (!member) return message.react("❌")
 
 let rol1 = ""
 let rol2 = ""
@@ -20,7 +19,7 @@ member.roles.add(rol3)
 member.roles.add(rol4)
 member.roles.add(rol5)
 member.roles.add(rol6)
-
+message.react("✅")
 let yetki2 = new Discord.MessageEmbed() 
 .setColor("#2F3136")
 .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
